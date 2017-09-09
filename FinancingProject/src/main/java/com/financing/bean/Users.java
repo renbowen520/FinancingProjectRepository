e24cab42f8f95816e5dcc6aba6da541de57b1134
renbowen <renbowen@HelloWorld>
2017-09-09 13:25:16 +0800

    实体类

--- FinancingProject/src/main/java/com/financing/bean/Users.java
+++ FinancingProject/src/main/java/com/financing/bean/Users.java
@@ -1,5 +1,18 @@
 package com.financing.bean;
 
-public class Users {
+import java.util.Date;
 
+public class Users {
+//用户基本表  
+	private  int id;
+	private String user_name;//用户名
+	private String  name;  //名称
+	private  String password;
+	private  String salt;//密码盐
+	private  String mobile_Phone;
+	private  int status;//'账号状态（0正常，1锁定，2删除）',
+	private int del_flag; //删除标志',
+	private int   identity;//身份',
+	private Date create_date;//创建时间
+	private Date update_date;//修改时间
 }
