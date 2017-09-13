cc96e56e3d4aa036d693aeb7adf0b962b68c00c4
renbowen <renbowen@192.168.88.1>
2017-09-13 14:51:50 +0800

    a

--- FinancingProject/src/main/java/com/financing/bean/Subject_order_record.java
+++ FinancingProject/src/main/java/com/financing/bean/Subject_order_record.java
@@ -9,17 +9,80 @@ import java.util.Date;
 public class Subject_order_record {
     
 	private int id;  //主键
-	private String serial_number;//
+	private String serial_number;//流水号
+	private int    deal_type; //交易类型
+	private BigDecimal amount;//订单金额
+	private int   status; //订单状态
+	private int  delflag;//是否删除
+	private Date create_date;//创建时间
+	private Date update_date;//更新时间
+	private Member member;//引用用户id
+	private Subject subject;//引用表的id
 	
-	private BigDecimal amount;
-	private Date createDate;
-	private byte dealType;
-	private byte delflag;
 	
-	private byte status;
-	private Date updateDate;
-	private Member member;
-	private Subject subject;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSerial_number() {
+		return serial_number;
+	}
+	public void setSerial_number(String serial_number) {
+		this.serial_number = serial_number;
+	}
+	public int getDeal_type() {
+		return deal_type;
+	}
+	public void setDeal_type(int deal_type) {
+		this.deal_type = deal_type;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getDelflag() {
+		return delflag;
+	}
+	public void setDelflag(int delflag) {
+		this.delflag = delflag;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+	public Date getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(Date update_date) {
+		this.update_date = update_date;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	public Subject getSubject() {
+		return subject;
+	}
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
 
+	
+	
 	
 }
