f4cc268e95afe5795dd60f8cc6d937814197e88c
renbowen <renbowen@192.168.88.1>
2017-09-13 16:16:56 +0800

    实体类更新

---
+++ FinancingProject/src/main/java/com/financing/bean/Push_notice.java
@@ -0,0 +1,52 @@
+package com.financing.bean;
+
+import java.util.Date;
+
+public class Push_notice {//公告表
+
+	private int id;   //主键
+	private String title;//标题
+	private String content;//内容
+	private int status;//公告状态
+	private Date create_date;//创建时间
+	private Date update_date;//修改时间
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+	public Date getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(Date update_date) {
+		this.update_date = update_date;
+	}
+	
+	
+	
+}
